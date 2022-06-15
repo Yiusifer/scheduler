1c55566c20ca9e3a22bc12d59a6c124b20cbfeb4
Adam Yiu <adam_yiu@outlook.com>
2022-06-14 23:18:13 -0400

    Building the DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,21 @@
 import React from "react";
+import DayListItem from "./DayListItem";
 
-const DayList = function() {
+const DayList = function(props) {
+
+  const DayListComponents = props.days.map(Day => {
+    return (
+      <DayListItem
+        id = {Day.id}
+        name = {Day.name}
+        spots = {Day.spots}
+        />
+    )
+  })
+
+
   return (
-    <ul></ul>
+    <ul> {DayListComponents} </ul>
   )
 }
 
